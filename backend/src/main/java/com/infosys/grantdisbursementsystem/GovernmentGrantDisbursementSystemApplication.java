af2a71115bedabd9a30b78ac42e203766d42c810
niharikapareek200@gmail.com <niharikapareek200@gmail.com>
2026-08-01 22:05:41 +0530

    Day 2: Tested DisbursementPlanService scheduling and release logic end-to-end, removed temporary test code

--- backend/src/main/java/com/infosys/grantdisbursementsystem/GovernmentGrantDisbursementSystemApplication.java
+++ backend/src/main/java/com/infosys/grantdisbursementsystem/GovernmentGrantDisbursementSystemApplication.java
@@ -1,13 +1,7 @@
 package com.infosys.grantdisbursementsystem;
 
-import com.infosys.grantdisbursementsystem.entity.Application;
-import com.infosys.grantdisbursementsystem.repository.ApplicationRepository;
-import com.infosys.grantdisbursementsystem.service.DisbursementPlanService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GovernmentGrantDisbursementSystemApplication {
@@ -15,5 +9,4 @@ public class GovernmentGrantDisbursementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(GovernmentGrantDisbursementSystemApplication.class, args);
     }
-    
 }
